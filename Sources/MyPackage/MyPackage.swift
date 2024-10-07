2e8bf93e13d62ad778b65f4f7c36e1a93e3c50a1
watakazu-git <132337028+watakazu-git@users.noreply.github.com>
2024-10-07 12:04:03 +0900

    Initial Commit

---
+++ Sources/MyPackage/MyPackage.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct MyLibrary {
+    public init() {}
+
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+}
